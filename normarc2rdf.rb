e2e1fc6f3ce9e30dad62aa77baa85558672b80d7
benjab <benjab@benjab-kontor.(none)>
2012-02-08 20:19:51 +0100

    added GPLv3 license

--- normarc2rdf.rb
+++ normarc2rdf.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby 
 # encoding: UTF-8
+=begin
+    NORMARC2RDF - a ruby program to convert binary MARC to RDF by YAML mapping
+    Copyright (C) 2012 Benjamin Rokseth
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
 require 'rubygems'
 require 'marc'
 require 'yaml'
