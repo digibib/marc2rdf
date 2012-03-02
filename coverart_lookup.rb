8ae119ec122758e3f6705a543c0b71b40c460336
benjab <benjab@benjab-kontor.(none)>
2012-03-02 08:31:39 +0100

    added debug to coverart_lookup

--- coverart_lookup.rb
+++ coverart_lookup.rb
@@ -3,6 +3,8 @@ require 'rest_client'
 require 'yaml'
 require 'nokogiri'
 require 'rdf'
+require 'rdf/rdfxml'
+require 'rdf/ntriples'
 require 'sparql/client'
 
 CONFIG           = YAML::load_file('config/config.yml')
@@ -13,6 +15,23 @@ COVERART_SOURCES = CONFIG['coverart_sources']
 
 @username = CONFIG['rdfstore']['username']
 @password = CONFIG['rdfstore']['password']
+
+def usage(s)
+    $stderr.puts(s)
+    $stderr.puts("Usage: \n")
+    $stderr.puts("#{File.basename($0)} -o output_file [-r recordlimit]\n")
+    $stderr.puts("  -r [number] stops processing after given number of records\n")
+    $stderr.puts("  -d debug output\n")
+    exit(2)
+end
+
+loop { case ARGV[0]
+    when '-r' then  ARGV.shift; $recordlimit = ARGV.shift.to_i # force integer
+    when '-d' then  ARGV.shift; $debug = true
+    when /^-/ then  usage("Unknown option: #{ARGV[0].inspect}")
+    else 
+    break
+end; }
 
   def fetch_cover_art(isbn)
     response = RestClient.get @url + isbn
@@ -43,6 +62,7 @@ COVERART_SOURCES = CONFIG['coverart_sources']
 
     @count = 0
     results.each do | solution |
+      
       cover_url = fetch_cover_art(solution.isbn.value)
       unless cover_url.empty?
         query = <<-EOQ
@@ -50,8 +70,9 @@ COVERART_SOURCES = CONFIG['coverart_sources']
         INSERT INTO <#{DEFAULT_GRAPH}> { <#{solution.book}> foaf:depiction <#{cover_url}> } 
         EOQ
 
+        if $debug then puts query end
+
         resource = RestClient::Resource.new(SPARUL_ENDPOINT, :user => @username, :password => @password)
-        # p query.inspect
         result = resource.post :query => query
         @count += 1
       end
@@ -70,8 +91,7 @@ COVERART_SOURCES.each do | source, sourcevalue |
   loop do    
     fetch_results(offset, limit)
     offset += limit
-    # insert break for testing:
-    # break if offset == 100 
+    if $recordlimit then break if @count > $recordlimit end
   end
 end
 p @count
