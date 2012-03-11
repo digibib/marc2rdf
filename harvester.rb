916f4f1711329fb2b4bc95056bf144ac541ef269
benjab <benjab@linux.local>
2012-03-11 03:03:09 +0100

    much improved harvester, takes both http and sparql sources

--- harvester.rb
+++ harvester.rb
@@ -11,7 +11,8 @@ require 'rdf/ntriples'
 require 'sparql/client'
 
 CONFIG           = YAML::load_file('config/config.yml')
-SOURCES          = YAML::load_file('config/harvesting.yml')
+HARVEST_CONFIG   = YAML::load_file('config/harvesting.yml')
+SOURCES          = HARVEST_CONFIG['sources']
 SPARQL_ENDPOINT  = SPARQL::Client.new(CONFIG['rdfstore']['sparql_endpoint'])
 SPARUL_ENDPOINT  = CONFIG['rdfstore']['sparul_endpoint']
 DEFAULT_PREFIX   = CONFIG['rdfstore']['default_prefix']
@@ -30,17 +31,20 @@ enc = "Basic " + Base64.encode64("#{username}:#{password}")
 def usage(s)
     $stderr.puts(s)
     $stderr.puts("Usage: \n")
-    $stderr.puts("#{File.basename($0)} -o output_file [-r recordlimit] [-d] \n")
+    $stderr.puts("#{File.basename($0)} [-f output_file] [-r recordlimit] [-o offset] [-s source] [-d] \n")
+    $stderr.puts("  -f [output_file] rdf output to file\n")    
     $stderr.puts("  -r [number] stops processing after given number of records\n")
     $stderr.puts("  -o [number] offset for start of harvest\n")    
+    $stderr.puts("  -s [source] limit harvest to this source\n")        
     $stderr.puts("  -d debug output\n")
     exit(2)
 end
 
 loop { case ARGV[0]
+    when '-f' then  ARGV.shift; $output_file = ARGV.shift
     when '-r' then  ARGV.shift; $recordlimit = ARGV.shift.to_i # force integer
-    when '-f' then  ARGV.shift; $output_file = ARGV.shift # force integer
     when '-o' then  ARGV.shift; $offset = ARGV.shift.to_i # force integer
+    when '-s' then  ARGV.shift; $source = ARGV.shift
     when '-d' then  ARGV.shift; $debug = true
     when /^-/ then  usage("Unknown option: #{ARGV[0].inspect}")
     else 
@@ -72,8 +76,6 @@ end; }
   end
   
   def rdfstore_lookup(offset, limit)
-  	isbns = []
-  	# query to return books without foaf:depiction
     query = <<-EOQ
     PREFIX bibo: <http://purl.org/ontology/bibo/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -108,26 +110,24 @@ if $debug then puts "book count: #{book_count.inspect}" end
 @count = 0
 if $output_file then $output_file = File.open($output_file, "a") end
 
-# let's harvest!
-SOURCES.each do | source, sourcevalue |
-  @source = source
-  @protocol = sourcevalue['protocol']
-  limit = sourcevalue['limit']
-  case @protocol
-  when 'http'
-    @prefix = sourcevalue['prefix']
-    @suffix = sourcevalue['suffix']
-    @apikey = sourcevalue['apikey']
-    @http_persistent = Net::HTTP::Persistent.new "#{@source}"
-    # next if @source == 'bokkilden'
-      # loops over source, uses url and limit from yaml
-
-    loop do    
-      rdf_result = rdfstore_lookup($offset, limit)
-      rdf_result.each do | solution |
+# loops over source, uses url and limit from yaml
+loop do    
+  # let's harvest!
+  @limit = HARVEST_CONFIG['options']['limit']
+  rdf_result = rdfstore_lookup($offset, @limit)
+  rdf_result.each do | solution |
+    SOURCES.each do | source, sourcevalue |
+      @statements = []
+      # if given source on commandline, limit to this source only
+      if $source then next if $source != source end
+      @protocol = sourcevalue['protocol']
+      if @protocol == 'http'
+        @prefix = sourcevalue['prefix']
+        @suffix = sourcevalue['suffix']
+        @apikey = sourcevalue['apikey']
+        @http_persistent = Net::HTTP::Persistent.new "#{source}"
         http_response = fetch_xpath_results(solution.isbn.value)
-        @statements = []
-        
+
         sourcevalue['harvest'].each do | predicate, conditions |
           obj = xml_harvest(http_response, :xpath => conditions['xpath'], :gsub => conditions['gsub'])
           unless obj.empty?
@@ -136,18 +136,40 @@ SOURCES.each do | source, sourcevalue |
             @statements << RDF::Statement.new(RDF::URI.new("#{solution.book}"), RDF.module_eval("#{predicate}"), obj)
             @count += 1
           end
-        end        
-        sparul_insert(@statements)
-      end #sourcevalue['harvest'].each
-      #continue to next loop iteration
-      $offset += limit
-      if $recordlimit then break if @count > $recordlimit end
-      break if @count >= book_count
-      sleep 5 # allow source 5 secs rest between harvests ...
-    end
-  when 'sparql'
-    puts "sparql"
-  end #end case @protocol
-end
+        end #sourcevalue['harvest'].each
+
+      elsif @protocol == 'sparql'
+        query = <<-EOQ
+        PREFIX bibo: <http://purl.org/ontology/bibo/>
+        SELECT * WHERE {
+          ?s bibo:isbn "#{solution.isbn.value}" ;
+             ?p ?o .
+        }
+        EOQ
+        endpoint = sourcevalue['endpoint']
+        sparql_client = SPARQL::Client.new("#{endpoint}", :headers => sourcevalue['headers'])
+        results = sparql_client.query(query, sourcevalue['options'])
+
+        results.each do | statement |
+          # harvest!
+          sourcevalue['harvest'].each do | predicate |
+            #p result[RDF.module_eval("#{predicate}")]
+            if statement[:p] == RDF.module_eval("#{predicate}")
+              @statements << RDF::Statement.new(RDF::URI.new("#{solution.book}"), RDF.module_eval("#{predicate}"), statement[:o])
+            end
+          end
+        end #end results.each       
+
+      end #end if @protocol
+      sparul_insert(@statements)
+    end #end SOURCES.each
+  end #end rdf_result.each
+  #continue to next loop iteration
+  $offset += @limit
+  if $recordlimit then break if @count > $recordlimit end
+  break if @count >= book_count
+sleep 5 # allow endpoint 5 secs rest between harvests ...
+end    
+
 p "count: #{@count}"
 if $output_file then $output_file.close end
