6d514d01ea15d78859e124b8b9fd399fbc114096
bensinober <bensinober@gmail.com>
2013-02-26 00:23:18 +0100

    rufus scheduler

--- lib/rules.rb
+++ lib/rules.rb
@@ -2,11 +2,15 @@
 # Struct for OAImodeler class 
 require "rufus/scheduler"
 
-Rule = Struct.new(:id, :tag, :name, :description, :start_time, :frequency, :script)
+Rule = Struct.new(:id, :scheduler, :job_id, :cron_id, :tag, :name, :description, :start_time, :frequency, :script)
 class Rule
 
   # a Rule is a SPARQL script to be run, either at intervals or at specified time
   # faraday connection can be overridden by passing a faraday object as :http arg
+  
+  def initialize
+    self.scheduler = Rufus::Scheduler.start_new
+  end
   
   def all
     rules = []
@@ -32,15 +36,16 @@ class Rule
   
   # new rule, repeated or frequent
   def create(params={})
-    self.id         = SecureRandom.uuid
-    # populate Library Struct    
+    # populate Rule Struct    
     self.members.each {|name| self[name] = params[name] unless params[name].nil? } 
+    self.id         = SecureRandom.uuid
     self.start_time = params[:start_time] ||= DateTime.now
     self
   end
   
   def update(params)
     return nil unless self.id
+    params.delete(:id)
     self.members.each {|name| self[name] = params[name] unless params[name].nil? }
   end
   
@@ -70,4 +75,48 @@ class Rule
   def reload
     self.find(:id => self.id)
   end  
+  
+  # routines to start/pause/stop and lookup Rufus::Scheduler rules
+  def start
+    self.job_id = self.scheduler.at "#{self.start_time}" do
+      # run script here
+    end
+  end
+  
+  # make job into schedule
+  def schedule
+    self.job_id.scheduler.cron "#{self.frequency}" do
+      # run script here
+    end
+  end
+  
+  def pause
+    self.job_id.pause
+  end
+
+  def resume
+    self.job_id.resume
+  end
+    
+  def unschedule
+    self.job_id.unschedule
+  end
+  
+  # returns a map job_id => job of at/in/every jobs  
+  def find_jobs
+    self.scheduler.jobs
+  end
+
+  def find_cron_jobs
+    self.scheduler.cron_jobs
+  end
+  
+  def find_all_jobs
+    self.scheduler.all_jobs
+  end
+
+  def find_jobs_by_tag(t)
+    self.scheduler.find_by_tag(t)
+  end
+  
 end
