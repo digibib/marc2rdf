e6dc23650808bca5ce809d4d2fbc4f93c413a779
bensinober <bensinober@gmail.com>
2013-06-14 16:03:27 +0200

    forgot auth.rb

---
+++ lib/auth.rb
@@ -0,0 +1,48 @@
+require 'sinatra/base'
+
+module Sinatra
+  module SessionAuth
+
+    module Helpers
+      def authorized?
+        session[:authorized]
+      end
+
+      def authorize!
+        redirect '/login' unless authorized?
+      end
+
+      def logout!
+        session[:authorized] = false
+      end
+    end
+
+    def self.registered(app)
+      app.helpers SessionAuth::Helpers
+
+      app.set :username, 'user'
+      app.set :password, 'secret'
+
+      app.get '/login' do
+        slim :login
+      end
+
+      app.get '/logout' do
+        session = {}
+        redirect '/'
+      end
+      
+      app.post '/login' do
+        if params[:user] == settings.username && params[:pass] == settings.password
+          session[:authorized] = true
+          redirect '/'
+        else
+          #session = {}
+          redirect '/login'
+        end
+      end
+    end
+  end
+
+  register SessionAuth
+end
