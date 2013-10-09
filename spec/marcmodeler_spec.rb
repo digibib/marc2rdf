b29e49ec7516daf6836491b1da90c2971debefe2
Benjamin Rokseth <bensinober@gmail.com>
2013-10-09 11:02:30 +0200

    use real sparql endpoint in marcmodeler

--- spec/marcmodeler_spec.rb
+++ spec/marcmodeler_spec.rb
@@ -3,6 +3,7 @@ describe MARCModeler do
 
   context "when converting RDF to MARCXML" do
     before(:each) do
+      @endpoint = SETTINGS["repository"]["sparul_endpoint"]
       default_graph = 'http://data.deichman.no/books'
       base = 'http://data.deichman.no/resource/'
       l = {'id'=>1, 'name'=>'test', 
@@ -10,7 +11,7 @@ describe MARCModeler do
       @library = l.to_struct("Library")
       @json = IO.read('./spec/example.sparqlresponse_manifestation.json')
       @emptyjson = IO.read('./spec/example.sparqlresponse_empty.json')
-      stub_request(:get, "http://datatest.deichman.no/sparql-auth/?format=application/sparql-results%2Bjson&query=SELECT%20*%20FROM%20%3Chttp://data.deichman.no/books%3E%20WHERE%20%7B%20%3Chttp://data.deichman.no/resource/tnr_583095%3E%20?p%20?o%20.%20%7D").
+      stub_request(:get, @endpoint + "?format=application/sparql-results%2Bjson&query=SELECT%20*%20FROM%20%3Chttp://data.deichman.no/books%3E%20WHERE%20%7B%20%3Chttp://data.deichman.no/resource/tnr_583095%3E%20?p%20?o%20.%20%7D").
          with(:headers => {'Accept'=>'application/sparql-results+json, application/sparql-results+xml'}).
          to_return(:status => 200, :body => @json, :headers => {'Content-Length' => 7038, 'Content-Type' => 'application/sparql-results+json'})
       @modeler = MARCModeler.new(@library)
@@ -26,7 +27,7 @@ describe MARCModeler do
       end
 
       it "should not fail when using an uri of non-existing resource" do
-        stub_request(:get, "http://datatest.deichman.no/sparql-auth/?format=application/sparql-results%2Bjson&query=SELECT%20*%20FROM%20%3Chttp://data.deichman.no/books%3E%20WHERE%20%7B%20%3Chttp://data.deichman.no/resource/tnr_dummy%3E%20?p%20?o%20.%20%7D").
+        stub_request(:get, @endpoint + "?format=application/sparql-results%2Bjson&query=SELECT%20*%20FROM%20%3Chttp://data.deichman.no/books%3E%20WHERE%20%7B%20%3Chttp://data.deichman.no/resource/tnr_dummy%3E%20?p%20?o%20.%20%7D").
           with(:headers => {'Accept'=>'application/sparql-results+json, application/sparql-results+xml'}).
           to_return(:status => 200, :body => @emptyjson, :headers => {'Content-Length' => 7038, 'Content-Type' => 'application/sparql-results+json'})
         @modeler.get_manifestation("http://data.deichman.no/resource/tnr_dummy")
